a7b15d22cb328b9b2eff53ada3883b3c6b2fd47b
Sohaib-50 <sohaibahmedabbasi0@gmail.com>
2021-07-05 14:43:15 +0500

    starting work of dataentry

--- sql_data_entry.py
+++ sql_data_entry.py
@@ -1,2 +1,18 @@
 import pandas as pd
 
+from os import getenv
+import mysql.connector
+
+
+
+db_connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=getenv("MySQLpassword"),
+    database="mobinfo"
+)
+
+db = db_connection.cursor()
+
+df = pd.read_csv("CleanedUp.csv")
+
