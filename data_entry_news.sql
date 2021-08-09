cec41600e0327826a8ba1914d47f13a37be0115d
Sohaib-50 <sohaibahmedabbasi0@gmail.com>
2021-08-09 20:31:51 +0500

    inserted some news to db

---
+++ data_entry_news.sql
@@ -0,0 +1,25 @@
+INSERT INTO news (heading, image_url, news_text)
+VALUES ("Xiaomi Mi Pad 5 appears in official teaser with keyboard accessory", 
+"https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-mi-pad-5-official-keyboard-poster/-1200x500m/gsmarena_001.jpg",
+"Xiaomi has dropped a new teaser for the Mi Pad 5 that's arriving on August 10. The new poster shows the Mi Pad 5's screen with rounded corners, and you can also see the keyboard cover case has a leather texture on the surface. Xiaomi has dropped a new teaser for the Mi Pad 5 that's arriving on August 10. The new poster shows the Mi Pad 5's screen with rounded corners, and you can also see the keyboard cover case has a leather texture on the surface. While we wait for Xiaomi to confirm a few more features of the Mi Pad 5 before its official unveiling on August 10, rumors claim the Chinese company will launch three tablets under the Mi Pad 5 series - Mi Pad 5, Mi Pad 5 Lite, and Mi Pad 5 Pro.
+
+All three slates are rumored to pack 10.9"" IPS LCDs with a 120Hz refresh rate and 240Hz touch sampling rate. However, the vanilla and Pro versions will be powered by the Snapdragon 870, whereas the Lite model will have a Snapdragon 860 SoC at the helm."
+);
+
+--@block
+INSERT INTO news (heading, image_url, news_text)
+VALUES ("Mi Smart Band 6 updated with flashlight function",
+"https://fdn.gsmarena.com/imgroot/news/21/08/mi-smart-band-6-firmware-update-flashlight/-1200w5/gsmarena_001.jpg",
+"Xiaomi’s Mi Smart Band 6 is now receiving a firmware update that brings a useful feature. With firmware version 1.0.4.38 a new flashlight feature has been added along with support for more languages. The update is now rolling out to UK, Czech Republic, and Kuwait, with more regions to follow soon. Before downloading the Mi Smart Band 6 update, you’ll need to make sure your MI Fit app is updated to version 5.3.0. Back in April, the Mi Smart Band 6 received a firmware update which added breathing monitoring during sleep tracking. This came with the app version 5.0.
+
+The Xiaomi MI Smart Band 6 has a 1.56-inch AMOLED touch screen and supports continuous hear rate monitoring, SpO2 monitoring, fitness tracking, music control, and alarm functions.");
+
+--@block
+INSERT INTO news (heading, image_url, news_text)
+VALUES ("Samsung Galaxy Z Flip3 has its full spec list leaked along with new renders",
+"https://fdn.gsmarena.com/imgroot/news/21/08/galaxy-z-flip3-full-specs/-1200/gsmarena_001.jpg",
+"A few hours ago a comprehensive leak gave us all the details regarding Samsung's upcoming Galaxy Z Fold3, and now it's time for the exact same thing to happen to the other foldable smartphone the company plans on unveiling at its August 11 event - the Galaxy Z Flip3. At this rate, we're not sure the event itself serves any purpose other than outing some promo videos. Anyway, let's dive right in. The Galaxy Z Flip3 will run Android 11 with One UI 3.1 on top, even though Google will probably release Android 12 to its Pixels within days or weeks after the Flip's announcement. That's how it goes with Samsung's August unveilings, though. The phone has a 6.7-inch internal display with 1080x2640 resolution and 120 Hz refresh rate, as well as a 1.9-inch 260x512 external screen. The glass 'back' that folds in half is covered in Gorilla Glass Victus. Running the show is the Qualcomm Snapdragon 888, paired with 8GB of RAM and 128/256GB of non-expandable UFS 3.1 storage. The main camera system consists of a 12 MP f/1.8 main sensor with OIS, and a 12 MP f/2.2 fixed-focus ultrawide. On the inside screen there's a 10 MP f/2.4 hole-punch selfie camera. The phone has a side-mounted fingerprint sensor, an IPx8 rating for water resistance (but not dust), dual-SIM support through one nanoSIM and one eSIM, and a 3,300 mAh battery. The device measures 166 x 72.2 x 6.9mm when opened, and weighs 183g.
+
+It's expected to start at €1,099 in the Eurozone, and possibly $1,099 in the US. It will be offered in Phantom Black, Cream, and Lavender color versions. Samsung has tested the hinge mechanism 200,000 times, which means you should be fine for more than five years of opening and closing it 100 times a day.
+
+");
